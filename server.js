539bc5a93f109d77c4ecc11b5d7a2dea608d1a7b
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2024-03-18 22:42:49 +0530

    disconnected frontend from server.js

--- server.js
+++ server.js
@@ -28,15 +28,19 @@ app.use(passport.session());
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
